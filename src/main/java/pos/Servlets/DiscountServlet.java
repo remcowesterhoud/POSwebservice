d350cf70f62037526abe04681bffd3a2200d2f4e
Laurens Oomen <oomen.laurens@gmail.com>
2015-09-23 16:03:14 +0200

    also added stuff
    even more stuff as remco
    added more comment stuff to the discountstuff

--- src/main/java/pos/Servlets/DiscountServlet.java
+++ src/main/java/pos/Servlets/DiscountServlet.java
@@ -1,27 +1,44 @@
 package pos.Servlets;
 
 import com.google.gson.Gson;
-import pos.Controllers.Inventory;
+import pos.Models.Discount;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.util.Date;
 
 /**
  * Created by Laurens Oomen on 22-9-2015 at 15:48.
  */
-@Path("/discounts")
-public class DiscountServlet {
-
+//@Path("/discounts")
+//public class DiscountServlet {
+//
+//    @Path("/{discountID}")
 //    @GET
 //    @Produces(MediaType.APPLICATION_JSON)
-//    public String getDiscount(@PathParam("barcode") int[] barcodes){
-//        Inventory inventory = Inventory.getInventory();
-//        Discount discount = inventory.searchDiscount(barcode);
+//    public String getDiscount(@PathParam("discountID") int discountID){
 //
+//        Discount discount = Discounts.searchDiscount(discountID);
 //        Gson gson = new Gson();
 //        return gson.toJson(discount);
 //    }
-}
+//    @Path("/discountDate")
+//    @GET
+//    @Produces
+//    public String getDiscountDate(@PathParam("date") Date date){
+//        Gson gson = new Gson();
+//        if(d.date = date) {
+//            for (Discount d : Discounts) {
+//
+//                gson.toJson(d);
+//
+//            }
+//        }
+//        return gson.toJson(date);
+//    }
+//    @Path("/newDiscount")
+//    @POST
+//    public void newDiscount(@PathParam("discount") Discount discount){
+//        Discounts.add(discount);
+//    }
+//}
